d3d7a366b362db5dd3a930a4a515cb3bd263f76f
Jitendra Singh <jitendra.singh@1rivet.com>
2024-02-06 13:40:11 +0530

    added git pratice file

---
+++ GitController.java
@@ -0,0 +1,9 @@
+package com.gitpractice.controller;
+
+public class GitComtroller {
+
+	public static void main(String[] args) {
+		System.out.println("abc");
+		System.out.println("abc");
+	}
+}
---
+++ src/main/java/com/gitpractice/controller/GitController.java
@@ -0,0 +1,8 @@
+package com.gitpractice.controller;
+
+public class GitController {
+
+	public static void main(String[] args) {
+		System.out.println("abd");
+	}
+}
